3224a0aafca56e2ef4b64b21b9d93819d0a1a2a8
Himasnhu Kumar <himanshukumarmpr09@gamil.com>
2024-08-05 20:39:07 +0530

    conversation

---
+++ conversation_operation.js
@@ -0,0 +1,27 @@
+// How to convert string => number, and other forms 
+// how.............?
+
+// let ... you have a value for string and you know this value is use for number how to convert
+let scoer = "77";
+console.log(scoer);
+// output: 77
+console.log(typeof scoer);
+// output is string 
+
+// to convert string to number you can use Number() function or + operator
+let convertNumber = Number(scoer);
+console.log(convertNumber);
+// output: 77
+console.log(typeof convertNumber);
+
+/*
+and you convert multple values in matible 
+form like stirng => number 
+number => string
+True => 1
+false => 0
+multipe
+
+*/
+// but this is not good method to convert it's a totsly wrong
+
