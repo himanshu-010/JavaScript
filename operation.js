949d3c590fb00ee44622008bdbb1acc89a4ffda8
Himasnhu Kumar <himanshukumarmpr09@gamil.com>
2024-08-05 21:09:34 +0530

    operation

---
+++ operation.js
@@ -0,0 +1,55 @@
+// what is operation
+// operation simply means  to perfroms some actions like add two number and so on
+// in this case we are going to perform addition operation
+console.log(2 + 6)
+console.log(2 * 6)
+console.log(2 - 6)
+console.log(2 / 6)
+console.log(2 % 6)
+
+// what is print of you write one string one one number ?
+console.log("4" + 7);
+// here we are going to print 47 because we are adding string and number together
+// similarly 
+console.log(6 + "8");
+//here give me output in 68 beacuse we are adding number and string together
+// so we can say that in js string is converted to number and vice versa
+// but we can't convert number to boolean and vice versa
+
+
+console.log(6+7+"78");
+// here we are going to print 1378 because we are adding number and string together
+
+//similarly 
+console.log("7"+7+90);
+// here we are going to print 7790 because we are adding string and number together
+
+// what is prefix or postfix 
+let value = 78
+value++;
+console.log(value);
+// here we are going to print 79 because we are incrementing the value of variable by
+++value;
+console.log(value);
+// here we are going to print 80 because we are incrementing the value of variable by
+// same like increment => decrement -- 
+
+
+
+
+let value2 = 90;
+--value2
+console.log (value2);
+// here we are going to print 89 because we are decrementing the value of variable by
+// same like -- => ++
+
+// operatuion one is add two variabes easly like
+let str1 ="Himanshu";
+let str2 = " Kumar";
+console.log(str1+str2);
+// here we are output become himanshu Kumar 
+
+
+
+
+
