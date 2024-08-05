8048d921327f7ff226f080279e9b884812fa9db1
Himasnhu Kumar <himanshukumarmpr09@gamil.com>
2024-08-05 19:14:48 +0530

    Varisbles

---
+++ Variable.js
@@ -0,0 +1,19 @@
+/*
+Data Types in Js 
+
+*/
+// const
+
+//there are three types of variables in JS
+const UserID = "956789";
+console.log(UserID);
+
+let name = "Himasnhu Kumar";
+console.log(name);  
+
+var salary = "$3000";
+console.log(salary);
+
+console.table([UserID,name,salary]);
+
+
