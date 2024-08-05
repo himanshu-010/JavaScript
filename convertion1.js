67b6d597c68df7715517771d32ee3b64cb1a4966
Himasnhu Kumar <himanshukumarmpr09@gamil.com>
2024-08-05 23:17:19 +0530

    some another changes

---
+++ convertion1.js
@@ -0,0 +1,28 @@
+// becically compare the some values to one to another like lets's take exmaple
+
+
+console.log(10 > 20);
+console.log(10 < 20);
+console.log(10 == 20);
+console.log(10 <= 20);
+console.log(10 >= 20);
+
+
+//check the string or number like
+
+console.log("56" > 89);
+// result fasle
+
+console.log("56" < 89);
+// result true
+
+
+// what is happen if compare a null > 0
+console.log(null > 0);
+console.log(null <= 0);
+console.log(null == 0);
+
+
+
+
+
