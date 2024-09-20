7e730c26433f9bebb9d85700a2a951c9765aa243
Himasnhu Kumar <himanshukumarmpr09@gamil.com>
2024-09-20 21:04:40 +0530

    Imediately run function

---
+++ Imdiately_Invoke_Function.js
@@ -0,0 +1,16 @@
+/************How to Run function Imediately  */
+
+
+( function him () {
+    console.log("Hy guys whats Up");
+}) () ; // this code run immediately 
+
+
+
+//*******Next method with arguments  */
+
+( (name) =>{
+    console.log(`Whats Your name :  ${name}`)
+}) ("HImanshu Sharma")
+
+
